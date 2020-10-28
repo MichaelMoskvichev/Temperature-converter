8c7bb6a50cd80645edc261ff8e79faeed695852a
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2020-10-28 20:55:01 +0300

    Initial Commit

---
+++ SwiftBookForthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookForthApp
+//
+//  Created by Миша Москвичев on 28.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
