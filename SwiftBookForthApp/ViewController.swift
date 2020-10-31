e96e2b92105984962c016ce9f4dc9f5665b13dde
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2020-10-31 20:25:34 +0300

    realised a controller

--- SwiftBookForthApp/ViewController.swift
+++ SwiftBookForthApp/ViewController.swift
@@ -12,9 +12,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var celiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLable: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
     @IBAction func sliderchanged(_ sender: UISlider) {
+        let temperaturCelsius = Int(round(slider.value))
+        celiusLabel.text = "\(temperaturCelsius)ºC"
+        
+        let fahrenheitTempetature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLable.text = "\(fahrenheitTempetature)ºF"
+        
     }
     
 }
